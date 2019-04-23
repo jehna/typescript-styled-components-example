70ef1b61d79730c04196cb0260b01afa8d99658a
Jesse Luoto <jesse.luoto@gmail.com>
2019-04-23 21:01:00 +0300

    Add styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
 import React from 'react'
+import styled from 'styled-components'
+
+export const MyStyledComponent = styled.div`
+  color: red;
+  border: 2px solid blue;
+`
 
 export const MyComponent = () => <div>Hello world</div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
-const path = require("path")
+const path = require('path')
 
 module.exports = {
-  mode: process.env.NODE_ENV || "development",
-  entry: "./src/index.tsx",
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
-    libraryTarget: "umd",
-    globalObject: "this"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+    libraryTarget: 'umd',
+    globalObject: 'this'
   },
-  devtool: process.env.NODE_ENV === "production" ? "none" : "eval-source-map",
+  devtool: process.env.NODE_ENV === 'production' ? 'none' : 'eval-source-map',
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"]
+    extensions: ['.ts', '.tsx', '.js', '.json']
   },
   module: {
-    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }]
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|dist)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: ['babel-plugin-styled-components']
+          }
+        }
+      }
+    ]
   }
 }
