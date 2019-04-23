c37f49ae8ae30c9013288685306bf8b7fbecae5f
Jesse Luoto <jesse.luoto@gmail.com>
2019-04-23 20:51:05 +0300

    Add bundling a react component with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path")
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+    libraryTarget: "umd",
+    globalObject: "this"
+  },
+  devtool: process.env.NODE_ENV === "production" ? "none" : "eval-source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }]
+  }
+}
